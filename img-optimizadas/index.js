e921c64dded61b59a8f2d521c15ec21626f98ff7
AndruDeveloper001 <anderson100js@gmail.com>
2024-07-19 11:42:49 -0600

    Mongo anderson

--- img-optimizadas/index.js
+++ img-optimizadas/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3009;
 const IP = '0.0.0.0'; // Escucha en todas las interfaces de red
 
 mongoose
-  .connect('mongodb+srv://progra01:procasa%40progra01@procasadev.wxejimf.mongodb.net/', {
+  .connect('mongodb+srv://anderson:anderson@cluster0.wh2v3jf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     // mongodb+srv://progra01:procasa%40progra01@procasadev.wxejimf.mongodb.net/    Progra01
     //'mongodb://localhost/procasa'
     //mongodb+srv://desjr:desjr@cluster0.qmiwvug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
