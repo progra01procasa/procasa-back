d1c5eec5888364a33e0c956b62e2751f3142e7fc
AndDev <Anderson100JS@gmail.com>
2024-07-26 11:08:05 -0600

    avance viernes

--- img-optimizadas/src/controllers/SoliVacaciones.controller.js
+++ img-optimizadas/src/controllers/SoliVacaciones.controller.js
@@ -1,26 +1,31 @@
 const SoliVacaciones = require("../models/SoliVacaciones.model")
 
 
-function crearSoliVacaciones(req,res){
+function crearSoliVacaciones(req, res) {
     let SoliVacacionesModel = new SoliVacaciones();
     let params = req.body;
     let user = req.user.sub;
+
+    // Asignar los valores al modelo
+    SoliVacacionesModel.IdUsuario = params.IdUsuario;
+    SoliVacacionesModel.codigo = params.codigo;
     // SoliVacacionesModel.nombre = req.user.nombre;
     // SoliVacacionesModel.Usuario = user;
     SoliVacacionesModel.fechaInicio = params.fechaInicio;
     SoliVacacionesModel.fechaFin = params.fechaFin;
-    SoliVacacionesModel.estado = params.estado;
+    SoliVacacionesModel.estado = "recibida";
     SoliVacacionesModel.tipofecha = params.tipofecha;
-    SoliVacacionesModel.save((err,SoliVacacionesGuardado)=>{
-        if(err){
-            return res.status(500).send({message:'error en la peticion'})
-        }else if(SoliVacacionesGuardado){
-            return res.status(200).send({data:SoliVacacionesGuardado})
-        }else{
-            return res.status(404).send({message:'no se pudo guardar'})
+    SoliVacacionesModel.save((err, SoliVacacionesGuardado) => {
+        if (err) {
+            return res.status(500).send({ message: 'Error en la petición' });
         }
-    })
+        if (SoliVacacionesGuardado) {
+            return res.status(200).send({ data: SoliVacacionesGuardado });
+        }
+        return res.status(404).send({ message: 'No se pudo guardar' });
+    });
 }
+
 
 
 
--- img-optimizadas/src/models/SoliVacaciones.model.js
+++ img-optimizadas/src/models/SoliVacaciones.model.js
@@ -5,16 +5,13 @@ const Schema = mongoose.Schema
 const SoliVacaciones = Schema({
     codigo: String,
     IdUsuario: { type: Schema.Types.ObjectId, ref: 'Usuarios'},
-    idDepatamento: { type: Schema.Types.ObjectId, ref: 'Usuarios'},
-    puesto: String,
-    dpi: String,
     diasDisponibles: Number,
     estado: String,
     fechaInicio: Date,
     fechaFin: Date,
     email: String,
     tipofecha: String,
-    respuesta: Schema.Types.Mixed
+    respuesta: String,
 })
 
 module.exports = mongoose.model("solivacaciones", SoliVacaciones)
