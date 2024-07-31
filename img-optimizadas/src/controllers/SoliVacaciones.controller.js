08e1e2b9c1705d747f49b0fae0d766fd0650a2c1
AndDev <Anderson100JS@gmail.com>
2024-07-31 15:43:29 -0600

    .

--- img-optimizadas/src/controllers/SoliVacaciones.controller.js
+++ img-optimizadas/src/controllers/SoliVacaciones.controller.js
@@ -1,12 +1,16 @@
-const SoliVacaciones = require("../models/SoliVacaciones.model");
+const SoliVacaciones = require("../models/SoliVacaciones.model")
 
-// Función para crear una nueva solicitud de vacaciones
+
 function crearSoliVacaciones(req, res) {
-    const SoliVacacionesModel = new SoliVacaciones();
-    const params = req.body;
+    let SoliVacacionesModel = new SoliVacaciones();
+    let params = req.body;
+    let user = req.user.sub;
 
+    // Asignar los valores al modelo
     SoliVacacionesModel.IdUsuario = params.IdUsuario;
     SoliVacacionesModel.codigo = params.codigo;
+    // SoliVacacionesModel.nombre = req.user.nombre;
+    // SoliVacacionesModel.Usuario = user;
     SoliVacacionesModel.fechaInicio = params.fechaInicio;
     SoliVacacionesModel.diasDisponibles = 0;
     SoliVacacionesModel.fechaFin = params.fechaFin;
@@ -14,51 +18,39 @@ function crearSoliVacaciones(req, res) {
     SoliVacacionesModel.tipofecha = params.tipofecha;
     SoliVacacionesModel.email = params.email;
     SoliVacacionesModel.respuesta = "";
-
     SoliVacacionesModel.save((err, SoliVacacionesGuardado) => {
         if (err) {
-            console.error('Error al guardar la solicitud de vacaciones:', err);
-            return res.status(500).send({
-                message: 'Error en la petición al guardar la solicitud de vacaciones',
-                error: err
-            });
+            return res.status(500).send({ message: 'Error en la petición' });
         }
         if (SoliVacacionesGuardado) {
-            return res.status(201).send({
-                data: SoliVacacionesGuardado
-            });
+            return res.status(200).send({ data: SoliVacacionesGuardado });
         }
-        return res.status(400).send({
-            message: 'No se pudo guardar la solicitud de vacaciones'
-        });
+        return res.status(404).send({ message: 'No se pudo guardar' });
     });
 }
 
-// Función para obtener las solicitudes de vacaciones por ID de usuario
-const obtenerSoliVacacionesxIdentidad = async (req, res) => {
-    try {
-        const idUser = req.user.sub;
-        const soliVacaciones = await SoliVacaciones.find({ IdUsuario: idUser }).exec();
 
-        if (!soliVacaciones || soliVacaciones.length === 0) {
-            return res.status(404).send({
-                message: `No se encontraron solicitudes de vacaciones para el usuario con id ${idUser}`
-            });
+
+
+const obtenerSoliVacacionesxIdentidad = (req, res) => {
+    let idUser = req.user.sub;
+    SoliVacaciones.find({ IdUsuario: idUser }, (err, soliVacaciones) => {
+        if (err) {
+            return res.status(500).send({ message: 'Error en la petición' })
         }
+        if (!soliVacaciones || soliVacaciones.length === 0) {
+            return res.status(404).send({ message: 'No se encontraron solicitudes de vacaciones para el usuario con id' });
+        }
+        return res.status(200).send({ data: soliVacaciones });
+    })
+}
 
-        return res.status(200).send({
-            data: soliVacaciones
-        });
-    } catch (err) {
-        console.error('Error al obtener las solicitudes de vacaciones:', err);
-        return res.status(500).send({
-            message: 'Error en la petición al obtener las solicitudes de vacaciones',
-            error: err
-        });
-    }
-};
 
-module.exports = {
+
+
+
+module.exports = { 
     crearSoliVacaciones,
     obtenerSoliVacacionesxIdentidad
-};
+}
+
